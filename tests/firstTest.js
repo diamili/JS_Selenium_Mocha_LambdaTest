bddd9740a851a4e617b99d4d68e18654b3cae7d3
Anastasia Titke <diamondmia@gmail.com>
2023-09-14 13:34:30 +0200

    implemented mocha

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,30 +1,42 @@
-const {Builder, By, Key} = require("selenium-webdriver");
-const assert = require("assert");
+const { Builder, By, Key } = require("selenium-webdriver");
 var should = require("chai").should()
 
-async function example(){
-//1 - launch the browser
-let driver = await new Builder().forBrowser("firefox").build();
+//describe block
+describe("add todo tests", function () {
 
-//2 - navigate to our app
-await driver.get("https://lambdatest.github.io/sample-todo-app/")
+    //it block
+    it("successfully adds a todo to applicatiom", async function () {
 
-//3 - add a todo
-await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
 
-//5 - asssert - to check that smth is presented on the page
-let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function(value){
-    return value
+            //1 - launch the browser
+            let driver = await new Builder().forBrowser("firefox").build();
+
+            //2 - navigate to our app
+            await driver.get("https://lambdatest.github.io/sample-todo-app/")
+
+            //3 - add a todo
+            await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
+
+            //5 - asssert - to check that smth is presented on the page
+            let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function (value) {
+                return value
+            });
+
+            //assert using chai should
+            todoText.should.equal("Learn Selenium")// return the error, becouse the strings do not match
+
+            //4 - close the browser
+            await driver.quit();
+        
+
+    });
+
 });
-//assert using node assertion
-//assert.strictEqual(todoText, "Learn Selenium") // strictEqual is used for comparison of 2 strings to check that they match
 
-//assert using chai should
-todoText.should.equal("Learn js")// return the error, becouse the strings do not match
-
-//4 - close the browser
-await driver.quit();
-}
-
-//calling the function
-example()
+//how to run the code
+//npx mocha --no-timeouts 'tests/*.js'
+//or it is better to add 'mocha --no-timeouts' into
+// package.json
+// "scripts": {
+//  "test": "mocha --no-timeouts"
+//and run as 'npm test'
