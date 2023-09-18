82bbc527932cba4c4a96fc63a5af862f3e5a083e
Anastasia Titke <diamondmia@gmail.com>
2023-09-18 18:49:02 +0200

    implementing of mocha

---
+++ capabilities.js
@@ -0,0 +1,14 @@
+var capabilities = {
+    "user" : "diamondmia",
+    "accessKey": "uuWGXZ7MQMrWnSKKgbnSAt01rk8bH2gc5PtBbXBdLXejHaGGlH",
+    "build": "Headless Build",
+    "name": "Selenium Test",
+    "platformName": "Windows 10",
+    "browserName": "Chrome",
+    "browserVersion": "92.0",
+    "headless": true
+}
+
+module.exports = {
+    capabilities
+};
--- tests/secondTest.js
+++ tests/secondTest.js
@@ -1,15 +1,43 @@
 const { Builder, By, Key } = require("selenium-webdriver");
-var should = require("chai").should()
+const ltCapabilities = require("../capabilities");
+var should = require("chai").should();
 
 //describe block
 describe("add second todo tests", function () {
+    var driver;
+
+    //username
+    const USERNAME = ltCapabilities.capabilities.user;
+
+    //key
+    const KEY = ltCapabilities.capabilities.accessKey;
+
+
+    //host
+    const GRID_HOST = "hub.lambdatest.com/wd/hub";
+
+    const gridUrl = "https://" + USERNAME + ":" + KEY + "@" + GRID_HOST;
+
+
+    beforeEach(function(){
+        ltCapabilities.capabilities.name = this.currentTest.title;
+        
+        driver = new Builder()
+        .usingServer(gridUrl)
+        .withCapabilities(ltCapabilities.capabilities)
+        .build();
+    });
+
+    afterEach(async function(){
+        await driver.quit();
+    });
 
     //it block
     it("successfully adds second todo to applicatiom", async function () {
         this.timeout(15000)
 
             //1 - launch the browser
-            let driver = await new Builder().forBrowser("firefox").build();
+            //let driver = await new Builder().forBrowser("firefox").build();
 
             //2 - navigate to our app
             await driver.get("https://lambdatest.github.io/sample-todo-app/")
@@ -23,10 +51,10 @@ describe("add second todo tests", function () {
             });
 
             //assert using chai should
-            todoText.should.equal(" Selenium")// return the error, becouse the strings do not match
+            todoText.should.equal("Learn Selenium")// return the error, becouse the strings do not match
 
             //4 - close the browser
-            await driver.quit();
+            //await driver.quit();
         
        
     });
